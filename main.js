ef44c7d97e63eab11409165cb76eaa9a3966ba1d
EveryoneInOne <jestemtapmadl@gmail.com>
2018-12-13 12:53:10 +0100

    koniec_ab2
    zakonczono_etap_dodawania_warstw

---
+++ main.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+   var mymap = L.map('mymap',
+                    {
+                     center: [52.1, 21.0], 
+                     zoom: 4,
+                     zoomControl:true,
+                     attributionControl:false
+                    }
+                    );
+    var lyrORTO = L.tileLayer.wms('http://mapy.geoportal.gov.pl/wss/service/img/guest/ORTO/MapServer/WMSServer', {
+        layers: "Raster", format:'image/png',transparent:"true",version: "1.1.1"
+    }
+                                  
+);
+    var lyrSOZO = L.tileLayer.wms('http://mapy.geoportal.gov.pl/wss/service/img/guest/SOZO/MapServer/WMSServer', {
+        layers: "Raster", format:'image/png',transparent:"true",version: "1.1.1",
+    }
+                               
+     );
+    
+    var lyrPRGWOJ = L.tileLayer.wms('http://localhost:8080/geoserver/ATE_Workspace/wms',
+                                {layers:'wojewodztwa', format: 'image/png',transparent:"true",version:"1.1.1",
+        
+        
+    }
+    
+     );
+    
+    
+    var lyrOSM = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
+    mymap.addLayer(lyrOSM);
+    
+    var BaseMaps = {"OpenStreetMaps":lyrOSM,"ORTOFTOMAPA":lyrORTO,"Mapa Sozologiczna":lyrSOZO,"Wykaz Wojewodztw":lyrPRGWOJ};
+    L.control.layers(BaseMaps).addTo(mymap);
+    L.control.scale({imperial:false}).addTo(mymap);
+});
